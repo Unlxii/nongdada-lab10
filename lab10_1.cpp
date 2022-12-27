cf95f21a4b05e08685f1446d9ebc3a3db19bf6c4
Nongdada <ronnachai_si@cmu.ac.th>
2022-12-27 16:18:42 +0700

    "complete"

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double a,b,c;
 	cout << "Enter initial loan: ";
+	cin >> a;
 	cout << "Enter interest rate per year (%): ";
+	cin >> b;
 	cout << "Enter amount you can pay per year: ";
+	cin >> c;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -21,13 +25,27 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	int EndOfYear = 1;
+	double sum ;
+	do{
+	cout << setw(13) << left << EndOfYear ;
+	cout << setw(13) << left << a ;
+	double Interest =a*(b/100);
+	cout << setw(13) << left << Interest ;
+	double total = a + Interest;
+	cout << setw(13) << left << total ;
+	if(total > c){
+		cout << setw(13) << left << c ;
+	}else{
+		c = total;
+		cout << setw(13) << left << c ;
+	}
+	double NewBalance = total-c;
+	cout << setw(13) << left << NewBalance ;
+	a = NewBalance;
+	sum = NewBalance;
 	cout << "\n";	
-	
+	EndOfYear ++;
+	}while(sum>0);
 	return 0;
 }
